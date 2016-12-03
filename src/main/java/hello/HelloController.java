5f0e7a4cc1809d4c5f9d49699c0ac2e93223f45f
Daimao <damian.koziel@pollub.edu.pl>
2016-12-03 11:15:14 +0100

    Zmiena Stringa w HelloController

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @RequestMapping("/")
     public String index() {
-        return "Pozdrowienia ze springBoota!";
+        return "Pozdrowienia ze springBoota!!";
     }
 
 }
