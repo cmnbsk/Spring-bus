b778e963497b60eef03430f1944d752b4511d1d7
darkbluegt <darkbluegt@gmail.com>
2016-12-21 17:08:12 +0100

    Zmienone nazwy kolumn w bazie

--- src/main/java/pl/pollub53/springBus/Application.java
+++ src/main/java/pl/pollub53/springBus/Application.java
@@ -12,15 +12,14 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
-//        System.out.println("Let's inspect the beans provided by Spring Boot (tłumaczenie: Sprawdźmy nasiona" +
-//                                                            " dostarczone przez jakąś sprężyne albo wiosne nwm):");
-//
-//        String[] beanNames = ctx.getBeanDefinitionNames();
-//        Arrays.sort(beanNames);
-//        for (String beanName : beanNames) {
-//            System.out.println(beanName);
-//        }
-        System.out.println("protip: wpisz w przegladarke localhost:8080/tajne-drzwi");
-    }
+        System.out.println("Let's inspect the beans provided by Spring Boot (tłumaczenie: Sprawdźmy nasiona" +
+                                                            " dostarczone przez jakąś sprężyne albo wiosne nwm):");
 
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+        //System.out.println("protip: wpisz w przegladarke localhost:8080/tajne-drzwi");
+    }
 }
